309ecc85f65867b182794061db08dbc57a01e3a7
ernicani <t.lindeker@h3hitema.fr>
2023-02-21 18:45:21 +0000

    uwu

--- srv.js
+++ srv.js
@@ -18,6 +18,7 @@ function md5(password) {
   return md5.digest('hex');
 }
 
+
 // genereate a random token
 function generateToken() {
   return crypto.randomBytes(20).toString('hex');
@@ -28,6 +29,8 @@ db.connect((err) => {
   if (err) {
     console.error('Error connecting to MySQL database: ', err);
     return;
+  }else {
+    console.log('Connected to MySQL database');
   }
 });
 
@@ -42,7 +45,7 @@ const io = require('socket.io')(http, {
 
 io.on('connection', (socket) => {
 
-  const sql = 'SELECT * FROM messages ORDER BY timestamp DESC LIMIT 20';
+  const sql = 'SELECT * FROM messages ORDER BY date_message DESC LIMIT 20';
   db.query(sql, (err, rows) => {
     if (err) {
       console.error('Error retrieving messages from database: ', err);
@@ -55,6 +58,7 @@ io.on('connection', (socket) => {
       timestamp: row.timestamp.toISOString().slice(0, 19).replace('T', ' '),
     }));
     socket.emit('history', messages);
+    console.log('client logged in');
   });
 
   socket.on('message', (message) => {
@@ -77,20 +81,46 @@ io.on('connection', (socket) => {
   socket.on('login', (user) => {
     const { email, password } = user;
     
-    const sql = 'SELECT * FROM users WHERE email = ? AND password = ?';
+    const sql = 'SELECT * FROM user WHERE user_email = ? AND user_password = ?';
     const values = [email, md5(password)];
     db.query(sql, values, (err, rows) => {
+      // if we connect then create a token and send it back to the client
       if (err) {
-        console.error('Error retrieving users from database: ', err);
+        console.error('Error retrieving user from database: ', err);
         return;
       }
       if (rows.length === 0) {
         socket.emit('login', {status: 'error', message: 'Invalid username or password'});
         console.log('Invalid username or password');
       } else {
-        socket.emit('login', {status: 'success',message: 'Login successful', token: rows[0].token, username: rows[0].username});
+        // create a token
+        const token = generateToken();
+        // update the token in the database
+        const sql = 'INSERT INTO user_token(token,user_id) VALUES (?, ?)';
+        const values = [token, rows[0].id];
+        db.query(sql, values, (err, result) => {
+          if (err) {
+            console.error('Error updating token in database: ', err);
+            return;
+          }
+          socket.emit('login', {status: 'success',message: 'Login successful', token: token, username: rows[0].username});
+        });
       }
     });
+    
+
+
+    //   if (err) {
+    //     console.error('Error retrieving users from database: ', err);
+    //     return;
+    //   }
+    //   if (rows.length === 0) {
+    //     socket.emit('login', {status: 'error', message: 'Invalid username or password'});
+    //     console.log('Invalid username or password');
+    //   } else {
+    //     socket.emit('login', {status: 'success',message: 'Login successful', token: rows[0].token, username: rows[0].username});
+    //   }
+    // });
   });
   
   socket.on('register', (user) => {
@@ -98,19 +128,19 @@ io.on('connection', (socket) => {
     const token = generateToken();
 
     // verify if user already exists
-    let sql = 'SELECT * FROM users WHERE email = ?';
+    let sql = 'SELECT * FROM user WHERE email = ?';
     let values = [email];
 
 
     db.query(sql, values, (err, rows) => {
         if (err) {
-            console.error('Error retrieving users from database: ', err);
+            console.error('Error retrieving user from database: ', err);
             return;
         }
         if (rows.length > 0) {
             socket.emit('register', {status: 'error', message: 'Email already exists'});
         } else {
-            sql = 'INSERT INTO users (email, username, password, token) VALUES (?, ?, ?, ?)';
+            sql = 'INSERT INTO user (email, username, password, token) VALUES (?, ?, ?, ?)';
             values = [email,username,md5(password),token];
             db.query(sql, values, (err, result) => {
                 if (err) {
@@ -127,60 +157,82 @@ io.on('connection', (socket) => {
 
     const token = server;
 
-    // verify if user already exists
-    let sql = 'select * from users where token = ?';
-
-
-    db.query(sql, [token], (err, rows) => {
+// optimised code : 
+    db.query('select * from user_token where token = ?', [token], (err, rows) => {
         if (err) {
-            console.error('Error retrieving users from database: ', err);
+            console.error('Error retrieving user from database: ', err);
             return;
         }
         if (rows.length === 0) {
           socket.emit('getServer', {status: 'error', message: 'Invalid id'});
         } else {
-            // get the server list
-            sql = 'SELECT * FROM servers';
-            db.query(sql, (err, rows) => {
+            db.query('select server_name,server.id from server inner join members on server.id = members.server_id where members.member_name = ?', [rows[0].user_id], (err, rows) => {
                 if (err) {
-                    console.error('Error retrieving servers from database: ', err);
+                    console.error('Error retrieving server from database: ', err);
                     return;
                 }
                 const serverList = rows.map((row) => ({
                     id: row.id,
-                    name: row.name,
+                    name: row.server_name,
                 }));
                 console.log(serverList);
-                socket.emit('getServer', {status: 'aaa', message: 'Server list', list: serverList});
+                socket.emit('getServer', {status: 'success', message: 'Server list', serverList: serverList});
             });
-
         }
     });
+  });
 
-    });
+
+
 
   socket.on('createServer', (server) => {
     const { name, token } = server;
 
-    let sql = 'Insert into servers (name, owner, members) values (?, ?, ?)';
+    // insert into server (server_name,owner_id,member_id) values ("test",(SELECT id FROM user WHERE user_name = 'ernicani'),(SELECT id from user where user_name = 'ernicani'));
+    let sql = 'SELECT user_id FROM user_token WHERE token = ?'
 
-    let values = [name, token, token];
-
-    console.log(values);
-    db.query(sql, values, (err, result) => {
+    db.query(sql, [token], (err, rows) => {
       if (err) {
-        console.error('Error inserting server into database: ', err);
+        console.error('Error retrieving user from database: ', err);
         return;
       }
-      socket.emit('createServer', {status: 'success', message: 'Server created'});
+      if (rows.length === 0) {
+        socket.emit('createServer', {status: 'error', message: 'Invalid id'});
+      } else {
+        let id = rows[0].user_id;
+        sql = 'INSERT INTO server (server_name,owner_id,member_id) VALUES (?, ?, ?)';
+        let values = [name,id,id];
+
+        db.query(sql, values, (err, result) => {
+          if (err) {
+            console.error('Error inserting server into database: ', err);
+            return;
+          }else {
+            sql = 'INSERT INTO members (member_name,server_id) VALUES (?,?)';
+            values = [id,result.insertId];
+            db.query(sql, values, (err, result) => {
+              if (err) {
+                console.error('Error inserting server into database: ', err);
+                return;
+              }
+              socket.emit('createServer', {status: 'success', message: 'Server created'});
+            });
+          }
+          
+        });
+      }
     });
+
   });
 
-    
-
-    
+  socket.on('getChannel', (channel) => {
+    const { serverId, token } = channel;
+    console.log(channel);
+  });
+  
 
   socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 
 
