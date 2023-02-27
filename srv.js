9866a4d5697f9a48e612e90938d1f4f6e04eeb76
Thomas Lindeker <tlindeker@gmail.com>
2023-02-27 15:08:52 +0000

    z

--- srv.js
+++ srv.js
@@ -181,6 +181,23 @@ io.on('connection', (socket) => {
       }
     });
 
+      socket.emit("me", socket.id)
+
+    socket.on("disconnect", () => {
+      socket.broadcast.emit("callEnded")
+      console.log("disconnected")
+    })
+
+    socket.on("callUser", (data) => {
+      io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+      console.log("callUser")
+    })
+
+    socket.on("answerCall", (data) => {
+      io.to(data.to).emit("callAccepted", data.signal)
+      console.log("answerCall")
+    })
+
 });
 
 
