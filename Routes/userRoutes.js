5818e1f7a47dd4e51256670ac13865a00eb2c095
Thomas Lindeker <tlindeker@gmail.com>
2023-04-06 11:04:58 +0000

    feat: add the Routes

---
+++ Routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const Router = express.Router();
+const userService = require('../services/userService');
+
+Router.post('/login', userService.userLogin);
+Router.post('/register', userService.userRegister);
+Router.post('/token', userService.userVerify);
+Router.post('/changePassword', userService.changePassword);
+Router.post('/resetPassword', userService.resetPasswordSend);
+Router.post('/reset', userService.resetPassword);
+
+
+module.exports = Router;
