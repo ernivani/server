1842eac45d47485b83fe967406e75d96e82a7fe3
Thomas Lindeker <tlindeker@gmail.com>
2023-04-06 11:04:07 +0000

    fix: remove unused files

--- utils/forgetPassword.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth : {
-        user : 'username',
-        password: 'password'
-    }
-});
--- utils/utils.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-function md5(password) {
-    let md5 = crypto.createHash('md5');
-    md5.update(password);
-    return md5.digest('hex');
-}
-
-module.exports = { md5 };
