03ba90056061239463dc3fa68584a3107e57b21b
Thomas Lindeker <tlindeker@gmail.com>
2023-04-08 23:19:30 +0000

    schéma relationel

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -18,3 +18,12 @@ connection.connect((err) => {
 );
 
 module.exports = connection;
+
+// user_information : contient les informations sur les utilisateurs.
+// servers : contient les informations sur les serveurs.
+// server_members : gère les relations entre les utilisateurs et les serveurs, y compris les rôles des membres.
+// roles : contient les rôles et leurs associations aux serveurs.
+// permissions : contient les permissions possibles pour les rôles.
+// role_permissions : gère les relations entre les rôles et les permissions.
+// channels : contient les informations sur les channels associés aux serveurs.
+// messages : contient les messages envoyés entre les utilisateurs.
