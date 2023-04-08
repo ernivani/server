9d1379bb9fe93a824817facf691250838a03f9b3
Thomas Lindeker <tlindeker@gmail.com>
2023-04-08 22:19:31 +0000

    sha512

--- config/cryptConfig.js
+++ config/cryptConfig.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
 function md5(password, salt= 'ernitropbg') {
-    let hash = crypto.createHash('md5');
+    let hash = crypto.createHash('sha512');
     hash.update(salt);
     hash.update(password);
     return hash.digest('hex');
