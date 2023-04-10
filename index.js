9dec850f6d447c17d1d2a9b60f565ad766b2f241
Thomas Lindeker <tlindeker@gmail.com>
2023-04-10 18:17:18 +0000

    socket io pour demande amis

--- index.js
+++ index.js
@@ -2,23 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const http = require("http");
-const socketIO = require("socket.io");
 const port = process.env.PORT || 3000;
 const user = require("./Routes/userRoutes.js");
 
 const server = http.createServer(app);
-const io = socketIO(server);
 
 const corsOptions = {
-    origin: "*",
-    optionsSuccessStatus: 200,
+    origin: "https://impin.fr",
+    methods: ["GET", "POST"],
 };
 
-// 
-
-
 app.use(express.json(), cors(corsOptions));
-
 
 app.use("/user", user);
 
@@ -26,38 +20,52 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to the API" }).status(200);
 });
 
-
-
 server.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 
+const jwt = require("jsonwebtoken");
 
+io.on("connection", (socket) => {
+    console.log("A user connected");
 
-// io.on("connection", (socket) => {
-//     console.log("A user connected with id:", socket.id);
+    // Décode le token JWT et attribue l'ID de l'utilisateur au socket
+    try {
+        const token = socket.handshake.query.token;
+        const decoded = jwt.verify(token, "ernitropbg");
+        socket.id = decoded.userId.toString();
+        console.log("New socket id:", socket.id);
+    } catch (err) {
+        console.log("Error:", err);
+    }
 
-//     socket.on("changeSocketId", (newSocketId) => {
-//         console.log(
-//             `Changing socket ID for user ${socket.id} to ${newSocketId}`
-//         );
-//         socket.id = newSocketId;
-//         console.log("New socket id:", socket.id);
-//         socket.emit("socketIdChanged", newSocketId);
-//     });
+    // Gestionnaire d'événements pour les demandes d'ami
+    socket.on("friend_request", (data) => {
+        console.log(`Friend request from ${socket.id} to ${data.receiverId}`);
 
-//     socket.on("sendMessage", ({ id, message, recipientId }) => {
-//         console.log(
-//             `Received message from user ${id} to ${recipientId}: ${message}`
-//         );
-//         io.sockets.sockets.forEach((socket) => {
-//             if (socket.id === recipientId) {
-//                 socket.emit("message", { id: id, message: message });
-//             }
-//         });
-//     });
+        // Parcourt la liste des sockets connectés pour trouver le destinataire
+        io.sockets.sockets.forEach((connectedSocket) => {
+            console.log(connectedSocket.id);
 
-//     socket.on("disconnect", () => {
-//         console.log("A user disconnected");
-//     });
-// });
+            // Si le socket correspond au destinataire, émet l'événement 'friend_request'
+            if (connectedSocket.id === data.receiverId) {
+                try {
+                    connectedSocket.emit("friend_request", data);
+                } catch (error) {
+                    console.log("Error while emitting friend_request:", error);
+                }
+            }
+        });
+    });
+
+    // Gestionnaire d'événements pour les déconnexions
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
