bebabe5153f83b5afc6293f13db4019d0436e49d
Thomas Lindeker <tlindeker@gmail.com>
2023-04-11 10:16:49 +0000

    organisation du code

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3000;
 const user = require("./Routes/userRoutes.js");
 const server = http.createServer(app);
 const jwt = require("jsonwebtoken");
-const connection = require("./config/dbConfig.js");
 const socket = require("socket.io");
 
 const corsOptions = {
@@ -25,7 +24,7 @@ server.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
 
-const io = socket(server, {
+global.io = socket(server, {
     cors: {
         origin: "*",
         credentials: true,
@@ -34,61 +33,21 @@ const io = socket(server, {
 
 global.onlineUsers = new Map();
 
-const add_user = (userId, socketId) => {
-    onlineUsers.set(socketId, userId);
-    console.log(onlineUsers);
-};
-
-const get_server_list = async (userId) => {
-    const query = `SELECT * FROM servers 
-    JOIN server_members ON servers.id = server_members.server_id 
-    WHERE server_members.user_id = ?`;
-    const params = [userId];
-    connection.query(query, params, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            // serverList = id name 
-            const serverList = result.map((server) => { return { id: server.id, name: server.name } });
-            chatSocket.emit("server-list", serverList);
-        }
-    });
-};
-
-const create_server = async (params) => {
-    const query = 'INSERT INTO servers (name, owner_id, created_at, updated_at) VALUES (?, ?, NOW(), NOW())';
-    const param = [params.serverName, params.userId];
-    connection.query(query, param, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            const serverId = result.insertId;
-            const query = 'INSERT INTO server_members (server_id, user_id, joined_at) VALUES (?, ?, NOW())';
-            const params = [serverId, param[1]];
-            connection.query(query, params, (err, result) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    const server = { id: serverId, name: param[0] };
-                    io.emit("server-created", server);
-                }
-            });
-        }
-    });
-};
-
-
-const disconnect_user = (socketId) => {
-    onlineUsers.delete(socketId);
-};
-
+const {add_user, get_server_list, create_server, disconnect_user} = require("./socket/request.js");
 
 io.on("connection", (socket) => {
-
     global.chatSocket = socket;
 
-    socket.on("add-user", (userId) => {add_user(userId, socket.id)});
-    socket.on("get-server-list", (userId) => {get_server_list(userId)});
-    socket.on("create-server", (params) => {create_server(params)});
-    socket.on("disconnect", () => {disconnect_user(socket.id)});
+    socket.on("add-user", (userId) => {
+        add_user(userId, socket.id);
+    });
+    socket.on("get-server-list", (userId) => {
+        get_server_list(userId);
+    });
+    socket.on("create-server", (params) => {
+        create_server(params);
+    });
+    socket.on("disconnect", () => {
+        disconnect_user(socket.id);
+    });
 });
---
+++ socket/request.js
@@ -0,0 +1,125 @@
+const connection = require("../config/dbConfig.js");
+
+const add_user = (userId, socketId) => {
+    onlineUsers.set(socketId, userId);
+    console.log(onlineUsers);
+};
+
+const get_server_list = async (userId) => {
+    const query = `SELECT s.id, s.name, GROUP_CONCAT(c.name) AS cname, GROUP_CONCAT(c.type) AS ctype, GROUP_CONCAT(c.id) AS cid
+    FROM servers s
+    JOIN server_members sm ON s.id = sm.server_id
+    JOIN channels c ON s.id = c.server_id
+    WHERE sm.user_id = ?
+    AND c.type = "text"
+    GROUP BY s.id
+    `;
+
+    const params = [userId];
+    connection.query(query, params, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // serverList = id name
+            const serverList = result.map((server) => {
+                return { id: server.id, name: server.name, cname: server.cname, ctype: server.ctype, cid: server.cid };
+            });
+            console.log(serverList);
+            chatSocket.emit("server-list", serverList);
+        }
+    });
+};
+
+const create_server = async (params) => {
+    const query =
+        "INSERT INTO servers (name, owner_id, created_at, updated_at) VALUES (?, ?, NOW(), NOW())";
+    const param = [params.serverName, params.userId];
+    connection.query(query, param, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const serverId = result.insertId;
+            const query =
+                "INSERT INTO server_members (server_id, user_id, joined_at) VALUES (?, ?, NOW())";
+            const params = [serverId, param[1]];
+            connection.query(query, params, async (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    const first_channel = await initialize_server(serverId);
+                    const server = { id: serverId, name: param[0], cname: first_channel.cname, ctype: first_channel.ctype, cid: first_channel.cid };
+                    io.emit("server-created", server);
+                }
+            });
+        }
+    });
+};
+
+const initialize_server = async (serverId) => {
+   
+
+    // Insert voice channel
+    const voiceChannelQuery =
+        'INSERT INTO channels (server_id, name, type, created_at, updated_at) VALUES (?, ?, "voice", NOW(), NOW())';
+    const voiceChannelParams = [serverId, "Voice Channel"];
+    connection.query(voiceChannelQuery, voiceChannelParams, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    // Insert category channel
+    const categoryChannelQuery =
+        'INSERT INTO channels (server_id, name, type, created_at, updated_at) VALUES (?, ?, "category", NOW(), NOW())';
+    const categoryChannelParams = [serverId, "Category Channel"];
+    connection.query(
+        categoryChannelQuery,
+        categoryChannelParams,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+        }
+    );
+     // Insert text channel
+     let cname;
+     let cid;
+     let ctype;
+     const textChannelQuery =
+         'INSERT INTO channels (server_id, name, type, created_at, updated_at) VALUES (?, ?, "text", NOW(), NOW())';
+     const textChannelParams = [serverId, "Text Channel"];
+    //  connection.query(textChannelQuery, textChannelParams, (err, result) => {
+    //      if (err) {
+    //          console.log(err);
+    //      } else {
+    //          cname = "Text Channel";
+    //          cid = result.insertId;
+    //          ctype = "text";
+    //          console.log(cname, cid, ctype);
+    //      }
+    //  });
+    return new Promise((resolve, reject) => {
+        connection.query(textChannelQuery, textChannelParams, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                cname = "Text Channel";
+                cid = result.insertId;
+                ctype = "text";
+                resolve({ cname, cid, ctype });
+            }
+        }
+        );
+    });
+};
+
+const disconnect_user = (socketId) => {
+    onlineUsers.delete(socketId);
+};
+
+module.exports = {
+    add_user,
+    get_server_list,
+    create_server,
+    disconnect_user,
+};
